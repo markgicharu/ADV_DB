cf2e71416f293829d668b2d1a640d0313d13ee70
markgicharu <markgicharu@hotmail.com>
2017-09-19 20:12:02 +0300

    Update storedprocedureandtrigger.sql

--- storedprocedureandtrigger.sql
+++ storedprocedureandtrigger.sql
@@ -6,3 +6,13 @@ as
 	select * from county
 /*execute procedure */
 	exec viewcountydata
+
+/*create procedure voterproc with parameters*/
+create procedure voterproc
+@constid int
+as 
+
+	select * from voter where constituencycode = @constid
+	
+/*execute voterproc with parameter*/
+exec voterproc 1
