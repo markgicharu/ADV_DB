e5941f258d9825da7b5d43a01d033f47b2fbf3e3
markgicharu <markgicharu@hotmail.com>
2017-09-19 18:30:27 +0300

    Update classwork.sql

--- classwork.sql
+++ classwork.sql
@@ -6,3 +6,17 @@ SELECT * from voter
 SELECT * from voter
 	WHERE EXISTS (SELECT constituencycode
 					FROM constituency)
+/*					
+SELECT * from voter
+	WHERE constituencycode(SELECT constituencycode
+								FROM constituency)
+									WHERE constituencycode = '2')*/
+									
+									
+select constituencycode from voter 
+WHERE EXISTS (SELECT constituencycode
+					FROM constituency)
+					
+select constituencycode from voter 
+WHERE NOT EXISTS (SELECT constituencycode
+					FROM constituency)
